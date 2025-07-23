bc35c5d42c90522443509a2aeb992fe9920917c0
devojyoti96 <devojyoti96@gmail.com>
2025-07-23 17:49:07 -0400

    Prefect local server is added

---
+++ meersolar/__init__.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+homedir = os.environ.get("HOME")
+if homedir is None:
+    homedir = os.path.expanduser("~")
+username = os.getlogin()
+cachedir = f"{homedir}/.solarpipe"
+ENV_FILE=f"{cachedir}/meersolar_prefect.env"
+load_dotenv(dotenv_path=ENV_FILE, override=False)
+
